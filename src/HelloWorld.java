676887533acec50fea771f3d0ef47aa94f9a45ee
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-03 08:21:50 -0600

    feat: final solution for intro to java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,6 @@
 /*todo: create a program that outputs "Hello, World!" to the console.
   todo: try renaming the main method. What happens when we try to run it? IT WONT RUN
   todo: refactor your code to use two system.out.print statements without changing the output
-  todo bonus: open a terminal and navigate to your project. change directories into the src directory (you should see your HelloWorld.java file). run the following command to compile
  */
 // the out directory is where IntelliJ puts the compiled Java files, to keep them separate from the source code
 // system.out.println is used to output information to the console with a newline
@@ -11,8 +10,8 @@
 
 public class HelloWorld {
     public static void main(String[] args) { //main HAS to be there or it won't run
-        System.out.print("Hello, World!");
-        System.out.print("Hello again, World!"); //this output is right next to the one above when I add a second System.out.print statement
+        System.out.println("Hello, World!");
+        System.out.println("Hello again, World!"); //this output is right next to the one above when I add a second System.out.print statement. if both statements are system.out.println then the outputs are on their own line
     }
 }
 
