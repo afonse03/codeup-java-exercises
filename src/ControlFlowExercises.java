7030f75ad29e5a4516f4b394d8b7e29a199beb76
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-07 10:06:36 -0600

    feat: added value in comment

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5; // integer variable with a value of
+        int i = 5; // integer variable with a value of 5
         while (i <= 15) { // this is a while loop that runs so long as i is less than or equal to 15
             System.out.println(i);
             i++; // each loop iteration, output the current value of i, then increment i by one
