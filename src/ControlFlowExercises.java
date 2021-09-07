210c7d3dc57ae4428a2ddf1a87a1f85917a58e98
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-07 11:12:20 -0600

    feat: solution for #2 in do while exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,14 +2,15 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 0;
+        //OJ's answer:
+        long i = 105;
         do {
+            i -= 5;
             System.out.println(i);
-            i += 2;
-        } while (i <= 100);
+        } while (i > -10);
     }
 }
 
 /*todo: Loop Basics - do while: 1. create a do while loop that will count by 2's starting with 0 and ending at 100 (follow each number with a new line)
-
+                                2. alter your loop to count backwards by 5's from 100 to -10
  */
