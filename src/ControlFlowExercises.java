9bf61edcbe390e356a2d6d818b2897776d5553f0
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-07 11:27:46 -0600

    feat: solution to #3 on do while exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,14 +3,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         //OJ's answer:
-        long i = 105;
+        int i = 2;
         do {
-            i -= 5;
             System.out.println(i);
-        } while (i > -10);
+            i *= i; //squaring a number in java can be done by multiplying the number by itself or you can use Math.pow() function
+        } while (i <= 10000); {
+            System.out.println(i);
+        }
     }
 }
 
 /*todo: Loop Basics - do while: 1. create a do while loop that will count by 2's starting with 0 and ending at 100 (follow each number with a new line)
                                 2. alter your loop to count backwards by 5's from 100 to -10
+                                3. create a do while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000
  */
