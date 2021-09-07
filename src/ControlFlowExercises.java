ac7c7ccb1cbb31d13d30b3af7cf0b51cc17893a6
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-07 13:38:35 -0600

    feat: solution for fizzbuzz question

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,22 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        for(int i = 5; i <= 15; i += 1) {
-//            System.out.println(i);
-//        }
-
+        for (int i = 1; i <= 100; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) { // is it a multiple of 3 & 5?
+                System.out.println("FizzBuzz");
+            } else if ((i % 3) == 0) { // is it a multiple of 3?
+                System.out.println("Fizz");
+            } else if ((i % 5) == 0) { // is it a multiple of 5?
+                System.out.println("Buzz");
+            } else System.out.println(i); // not a multiple of either 3 or 5
+        }
+    }
 }
 
-/*todo: Loop Basics - for: 1. refactor the previous two exercises to use a for loop instead (I couldn't complete refactoring the second exercise)
+//source: https://howtodoinjava.com/java/puzzles/fizzbuzz-solution-java/
+
+/*todo: Fizzbuzz - 1. write a program that prints the numbers from 1 to 100
+                   2. for multiples of three: print "fizz" instead of the number
+                   3. for the multiples of five: print "buzz"
+                   4. for numbers which are multiples of both three and five: print "FizzBuzz"
  */
