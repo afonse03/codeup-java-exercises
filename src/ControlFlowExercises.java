9f4d1dee3695b3d2f125db06330d13c3c3a07357
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-07 10:45:31 -0600

    feat: solution for #1 in do while exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,16 +2,14 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5; // integer variable with a value of 5
-        while (i <= 15) { // this is a while loop that runs so long as i is less than or equal to 15
+        int i = 0;
+        do {
             System.out.println(i);
-            i++; // each loop iteration, output the current value of i, then increment i by one
-        }
+            i += 2;
+        } while (i <= 100);
     }
 }
 
-/*todo: Loop Basics - while: 1. create an integer variable i with a value of 5
-                             2. create a while loop that runs so long as i is less than or equal to 15
-                             3. each loop iteration, output the current value of i, then increment i by one
-  output should be: 5 6 7 8 9 10 11 12 13 14 15
+/*todo: Loop Basics - do while: 1. create a do while loop that will count by 2's starting with 0 and ending at 100 (follow each number with a new line)
+
  */
