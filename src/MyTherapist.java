4baac95f977b7a866fd8114d733f7c6e2220cd8d
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-08 08:33:14 -0600

    feat: final solution for warmup on 9/8/21

--- src/MyTherapist.java
+++ src/MyTherapist.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class MyTherapist {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Hi Amanda! I'm your virtual therapist. On a scale of 1-10, 10 being great, how are you doing today?");
+        System.out.print("Hi! I'm your virtual therapist. On a scale of 1-5, 5 being great, how are you doing today?");
         System.out.println(" ");
         int userResponseToTherapist = Integer.parseInt(String.valueOf(scanner.nextInt()));
-        for (int i = 1; i <= userResponseToTherapist; i++) {
+        if (userResponseToTherapist == 1) {
+            System.out.println("Oh no...let's work on it!");
+        } else if (userResponseToTherapist == 2) {
+            System.out.println("Ok let's talk more about it!");
+        } else if (userResponseToTherapist == 3) {
+            System.out.println("Ok not bad!");
+        } else if (userResponseToTherapist == 4) {
+            System.out.println("Awesome, you're doing great!");
+        } else if (userResponseToTherapist == 5) {
+            System.out.println("That's perfect!");
+        } else {
+            System.out.println("Oops, please answer between 1-5");
         }
-        System.out.println("Thanks for letting me know!");
     }
 }
