ba17c26a5ea88fa94c222b7078b72ae3d6e3436a
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-09 19:04:48 -0600

    feat: final solution for string exercise

--- src/Bob.java
+++ src/Bob.java
@@ -4,14 +4,15 @@ public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userQuestion = scanner.nextLine().toLowerCase();
-        System.out.println(userQuestion);
         if (userQuestion.endsWith("?")) {
             System.out.println("Sure");
         } else if (userQuestion.endsWith("!")) {
             System.out.println("Whoa, chill out!");
+        } else if (userQuestion == ("")) {
+            System.out.println("Fine. Be that way!");
         } else {
             System.out.println("Whatever");
         }
     }
 }
-//CONTINUE WORKING ON THIS EXERCISE AND SUBMIT TO GITHUB TONIGHT!!!!
+
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class MethodsExercises {
     // todo: basic arithmetic
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println(getAddition(10, 10));
         System.out.println(getSubtraction(20, 10));
         System.out.println(getMultiplication(10, 10));
@@ -9,27 +9,30 @@ public class MethodsExercises {
         System.out.println(getModulus(10, 6));
         System.out.println(getInteger(3, 5));
 
-
     }
 
-    public static int getAddition (int x, int y) {
+    public static int getAddition(int x, int y) {
         return x + y;
     }
-    public static int getSubtraction (int x, int y) {
+
+    public static int getSubtraction(int x, int y) {
         return x - y;
     }
-    public static int getMultiplication (int x, int y) {
+
+    public static int getMultiplication(int x, int y) {
         return x * y;
     }
-    public static int getDivision (int x, int y) {
+
+    public static int getDivision(int x, int y) {
         return x / y;
     }
-    public static int getModulus (int x, int y) {
+
+    public static int getModulus(int x, int y) {
         return x % y;
     }
     // todo: create a method that validates that user input is in a certain range (STILL WORKING ON IT)
 
-    public static int getInteger (int min, int max) {
+    public static int getInteger(int min, int max) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number between 1 and 10: ");
         int userInput = scanner.nextInt();
@@ -39,4 +42,17 @@ public class MethodsExercises {
             return userInput;
         }
     }
+
+    // todo: calculate the factorial of a number
+
+//    public static int factorial(int n) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a number between 1 and 10: ");
+//        int input = scanner.nextInt();
+//        return input;
+//
+//        if (input = 1; input <= 10; input++) {
+//            return input * input;
+//        }
+//    }
 }
