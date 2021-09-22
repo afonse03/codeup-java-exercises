099087462097c37b37c345aa9d4cd96284faca8d
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-22 09:10:30 -0600

    feat: working on warmup 9/22/21

--- src/MathTest.java
+++ src/MathTest.java
@@ -1,8 +1,10 @@
-//public class MathTest {
-//    int mathTestInt = -23;
-//    int absoluteValueTest = Math.abs(mathTestInt);
-//
-//    public static void main(String[] args) {
+public class MathTest {
+    int mathTestInt = -23;
+    int absoluteValueTest = Math.abs(mathTestInt);
+
+    public static void main(String[] args) {
 //        System.out.println(absoluteValueTest);
-//    }
-//}
+        int[] myArray = {3, 4, 5};
+        System.out.println(MyMathMethods.arraySum(myArray));
+    }
+}
--- src/MyMathMethods.java
+++ src/MyMathMethods.java
@@ -3,7 +3,39 @@
 
 
 public class MyMathMethods {
-    public int multiply(int x, int y) {
+//    public static int multiply(int x, int y) {
+//        return x * y;
+//    }
+
+    // warmup on 9/22/21
+    // 1. use method overloading to create a public static multiply method that works with doubles
+    public static double multiply(double x, double y) {
         return x * y;
     }
+
+    // 2. write a public static method named quotient that works with both integers and doubles (use mthod overloading) and returns the result of dividing the two passed numbers.
+//    public static int quotient(int x, double y) {
+//        return (int) (x / y);
+//    }
+
+    public static int quotient(int x, int y) {
+        return x / y;
+    }
+
+    public static double quotient(double x, double y) {
+        return x / y;
+    }
+    // 3. write a public static method named arraySum that takes in an array of int numbers (not a list, an array), and returns the sum of the numbers
+    public static int arraySum(int[] myNumArray) {
+        int sum = 0;
+        for (int i = 0; i < myNumArray.length; i++) {
+            sum += myNumArray[i];
+        }
+        return sum;
+    }
+
+    // 4: create a class called Movie. The class should define two instance variables of type String named name and director. Add a constructor that takes two strings and sets the name and director properties based on the passed arguments. Make it so the instance variables are not accessible from outside the Movie class. Write getters and setters for the instance variables.
+
+    // BONUS: if the passed name or director arguments are null or empty, the constructor should throw an IllegalArgumentException
+
 }
