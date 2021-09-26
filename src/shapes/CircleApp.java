7347de2d5aa06e893fecd0a8630aa4e6e8b9ec0c
afonse03 <amanda.m.fonseca03@gmail.com>
2021-09-25 22:57:21 -0600

    feat: working solution for shapes

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,11 @@
+package shapes;
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        System.out.println("What is the radius of the circle?");
+        Input input = new Input();
+        double radius = input.getDouble();
+        Circle newCircle = new Circle(radius);
+    }
+}
