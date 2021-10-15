4ef6efe7758b571b22584c2f5b2f65a84364b383
afonse03 <amanda.m.fonseca03@gmail.com>
2021-10-14 21:30:27 -0600

    feat: final solution for warmup on 10/14

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -13,10 +13,28 @@
 // work in progress
 public class JavaDrills {
     public static String flipOuterCase(String word) {
-        char[] chars = text.toCharArray();
+        char[] chars = word.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            if (Character.isUpperCase(c))
+            boolean firstAndLast = chars[i] == chars[0] || chars[i] == chars[chars.length - 1]; // this is how you select the first and last letter of the string
+            if (Character.isUpperCase(c)) {
+                if (firstAndLast) {
+                    chars[i] = Character.toLowerCase(c);
+                }
+            } else if (Character.isLowerCase(c)) {
+                if (firstAndLast) {
+                    chars[i] = Character.toUpperCase(c);
+                }
+            }
         }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(flipOuterCase("cat"));
+        System.out.println(flipOuterCase("CaT"));
+        System.out.println(flipOuterCase("caT"));
+        System.out.println(flipOuterCase("cAt"));
+        System.out.println(flipOuterCase("AmandA"));
     }
 }
--- src/warmup/Marker.java
+++ src/warmup/Marker.java
@@ -2,5 +2,5 @@ package warmup;
 import java.util.ArrayList;
 import java.util.List;
 abstract class Marker {
-    public void write();
+//    public void write();
 }
