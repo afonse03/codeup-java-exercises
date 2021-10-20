3b659a043f31ae5387dffae081b4ef833850bc18
afonse03 <amanda.m.fonseca03@gmail.com>
2021-10-20 17:10:17 -0600

    feat: working solution for exceptions and error handling

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,11 +1,11 @@
-//import util.Input;
-//public class InputTest {
-//    public static void main(String[] args) {
-//        Input input = new Input();
-//        String userInput = input.getString();
-//        System.out.println(userInput);
-//    }
-//}
+import util.Input;
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+        String userInput = input.getString();
+        System.out.println(userInput);
+    }
+}
 /*
 albert wrote this: where do I put it?
 
