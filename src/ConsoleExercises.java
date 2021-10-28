70ca58f006f66345f3c4d2442b4d59a4a4488477
afonse03 <amanda.m.fonseca03@gmail.com>
2021-10-27 22:06:22 -0600

    feat: working solution for console io exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,5 +1,14 @@
 public class ConsoleExercises {
     public static void main(String[] args) {
+        String name = "Panda";
+        System.out.printf("Hello there, %s. Nice to see you. \n", name); // the %s will be replaced with the value of the name variable
+
+        // multiple placeholders
+        String greeting = "Salutations";
+        String name2 = "Panda";
+        System.out.printf("%s, %s! ", greeting, name2);
+
+
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %.2f.", pi);
     }
